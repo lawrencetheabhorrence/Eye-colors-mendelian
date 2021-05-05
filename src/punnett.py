3a27dcd7821ca85ed8c358ca7f501bd1481aebe3
lawrencetheabhorrence <luluemilianolawrence@gmail.com>
2021-05-05 21:30:39 +0800

    initial commit

---
+++ src/punnett.py
@@ -0,0 +1,7 @@
+import random
+
+def generate_parents(){
+    return ''.join([random.choice(['B', 'b'], random.choice(['B', 'b']), random.choice(['G', 'g']), random.choice(['G', 'g'])
+
+def punnett() {
+    
